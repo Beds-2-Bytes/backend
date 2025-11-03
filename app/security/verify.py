177fc6d43bd11254ac9839db8e7c1e5b18e1313b
Hermes <heikkihe@arcada.fi>
2025-11-03 18:25:32 +0200

    cleanup

--- app/security/verify.py
+++ app/security/verify.py
@@ -1,15 +1,3 @@
-'''from fastapi import HTTPException, Header, Depends
-from jose import jwt, ExpiredSignatureError, JWTError
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-ALGORITHM = "HS256"
-SECRET_KEY = os.getenv("SECRETKEY")
-'''
-#print(repr(os.getenv("SECRETKEY")))
-
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt, ExpiredSignatureError, JWTError
@@ -38,23 +26,3 @@ def verify_jwt_token(credentials: HTTPAuthorizationCredentials = Depends(securit
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-
-
-'''def verify_jwt_token(authorization: str = Header(...)):
-    """Verify JWT from 'Authorization: Bearer <token>' header."""
-    if not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=401, detail="Invalid authorization header format")
-
-    token = authorization.split("Bearer ")[1]
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id = payload.get("sub")
-        if not user_id:
-            raise HTTPException(status_code=401, detail="Invalid token payload")
-        return user_id
-    except ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Token has expired")
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
-'''
