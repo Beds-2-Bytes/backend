7f900c692e5a79a9d17e96df1be4aa55212f8b85
Hermes <heikkihe@arcada.fi>
2025-11-10 14:29:04 +0200

    edit to db link

--- app/database/database.py
+++ app/database/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = f"postgresql://{os.getenv('DBUSER')}:{os.getenv('DBPW')}@localhost:5432/{os.getenv('DBNAME')}"
+DATABASE_URL = f"postgresql://{os.getenv('DBUSER')}:{os.getenv('DBPW')}@{os.getenv('DBURL')}/{os.getenv('DBNAME')}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
